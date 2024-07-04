6834a8defad9c354f4cf2d5c4fb7ab292674d19c
Gabriel Kloppel <tkloppel@gmail.com>
2024-07-03 21:18:00 -0300

    Terminal_Argumentos

---
+++ Terminal_Argumentos/src/conteudo.java
@@ -0,0 +1,34 @@
+    public class conteudo {
+        /*                         Terminal e Argumentos
+    Nem sempre executamos nosso programa Java pela IDE, já pensou nossos clientes tendo que instalar o Eclipse 
+    ou VsCode para rodar o sistema em sua empresa ?
+    Com a JVM devidamente configurada, nós podemos criar um executável do nosso programa e disponibilizar o 
+    instalador para qualquer sistema operacional.
+    No nosso caso iremos aprender como executar um programa Java via terminal como MS - DOS ou terminal do VsCode.
+    Vamos criar uma classe chamada MinhaClasse.java com o código abaixo:
+        */
+    
+    //Observe que nosso projeto Java criado por um IDE, ele terá uma pasta chamada bin.
+    // É nesta pasta que ficarão os arquivos .class, o nosso bytecode.
+    
+    /* Mesmo usando uma IDE, nós sempre precisaremos identificar aonde se encontram as classes do nosso projeto,
+    no meu caso está em: C:\Users\Gabriel - Sol\Documents\Estudos\dio_trilha_java_basico-1\Terminal_Argumentos.
+    */
+    
+    /*               Terminal
+    Vamos ilustrar como executar uma classe, depois de compilada, sem precisar usar a IDE.
+    1. Abra o MS-DOS ou Power Shell
+    2. Localize o diretório do seu projeto: 
+    cd C:\Users\Gabriel - Sol\Documents\Estudos\dio_trilha_java_basico-1\Terminal_Argumentos
+    3. Acesse a pasta **** bin: ** cd bin**
+    4. Agora digite o comando:**java --enable-preview MinhaClasse
+    ** (nome da sua classe sem a extensão .class) */
+    
+    /*               Argumentos
+    Quando executamos uma classe que contém o método main, o mesmo permite que passemos um array []de argumentos 
+    do tipo String. Logo podemos após a definição da classe a ser elaborada informar estes parâmetros, exemplo:
+    
+    java MinhaClasse agumentoUm argumentoDois */
+    
+    }
+
