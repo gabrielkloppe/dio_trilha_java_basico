8a1d5bec15a3f2a0a334018eba740ffe5818a44b
Gabriel Kloppel <tkloppel@gmail.com>
2024-07-11 15:46:40 -0300

    teste de Terminal_Argumentos

--- Terminal_Argumentos/src/MinhaClasse.java
+++ Terminal_Argumentos/src/MinhaClasse.java
@@ -1,6 +1,6 @@
 public class MinhaClasse {
     
     public static void main(String[] args) {
-        System.out.println("Oi turma, fui executado pelo Terminal");
+        System.out.println("Oi turma, tudo bem com vocês?,  fui executado pelo Terminal");
 }
  }
